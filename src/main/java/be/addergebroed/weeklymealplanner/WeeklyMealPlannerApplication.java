5358cc534c4de12d9b2a69187d1561339596f808
Kevin Uyttebroeck <uyttebroeckkevin@gmail.com>
2023-11-30 10:02:57 +0100

    initial commit

---
+++ src/main/java/be/addergebroed/weeklymealplanner/WeeklyMealPlannerApplication.java
@@ -0,0 +1,13 @@
+package be.addergebroed.weeklymealplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklyMealPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeeklyMealPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/addergebroed/weeklymealplanner/WeeklyMealPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package be.addergebroed.weeklymealplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeklyMealPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
