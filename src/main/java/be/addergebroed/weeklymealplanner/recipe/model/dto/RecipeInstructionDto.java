ae0f4b7765e17209a9ed75693243dcb75d8e8dcd
Kevin Uyttebroeck <uyttebroeckkevin@gmail.com>
2023-12-07 14:44:24 +0100

    remove id from RecipeInstructionDto

--- src/main/java/be/addergebroed/weeklymealplanner/recipe/model/dto/RecipeInstructionDto.java
+++ src/main/java/be/addergebroed/weeklymealplanner/recipe/model/dto/RecipeInstructionDto.java
@@ -2,10 +2,9 @@ package be.addergebroed.weeklymealplanner.recipe.model.dto;
 
 import be.addergebroed.weeklymealplanner.recipe.model.RecipeInstruction;
 
-public record RecipeInstructionDto(Long id, int step, String instruction) {
+public record RecipeInstructionDto(int step, String instruction) {
     public static RecipeInstructionDto convertToDto(RecipeInstruction instruction){
       return new RecipeInstructionDto(
-              instruction.getId(),
               instruction.getStep(),
               instruction.getInstruction()
       );
