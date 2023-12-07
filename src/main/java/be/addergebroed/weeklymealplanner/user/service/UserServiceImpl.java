90355320df32fe78bb03d4ba480274b0ec600479
Kevin Uyttebroeck <uyttebroeckkevin@gmail.com>
2023-12-07 13:37:22 +0100

    add fetch all user & fetch user by id

--- src/main/java/be/addergebroed/weeklymealplanner/user/service/UserServiceImpl.java
+++ src/main/java/be/addergebroed/weeklymealplanner/user/service/UserServiceImpl.java
@@ -1,11 +1,28 @@
 package be.addergebroed.weeklymealplanner.user.service;
 
+import be.addergebroed.weeklymealplanner.user.model.User;
 import be.addergebroed.weeklymealplanner.user.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepo;
+
+    @Override
+    public List<User> fetchAllUsers() {
+        return userRepo.findAll();
+    }
+
+    @Override
+    public User fetchUserById(Long id) {
+        if (id==null){
+            throw new IllegalArgumentException("Id can not be null");
+        }
+        return userRepo.findById(id).orElseThrow(() -> new EntityNotFoundException("Could not find user with id " + id));
+    }
 }
