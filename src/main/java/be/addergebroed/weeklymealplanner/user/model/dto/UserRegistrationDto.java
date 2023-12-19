f3841936d66bf97e48c0598f56192c4c14ec21a4
Kevin Uyttebroeck <uyttebroeckkevin@gmail.com>
2023-12-19 20:28:01 +0100

    add covertToDto method

--- src/main/java/be/addergebroed/weeklymealplanner/user/model/dto/UserRegistrationDto.java
+++ src/main/java/be/addergebroed/weeklymealplanner/user/model/dto/UserRegistrationDto.java
@@ -4,7 +4,16 @@ import be.addergebroed.weeklymealplanner.user.model.User;
 
 
 public record UserRegistrationDto(
+
         String email,
+
         String password) {
+
+    public static UserRegistrationDto convertToDto(User user) {
+        return new UserRegistrationDto(
+                user.getEmail(),
+                user.getUsername()
+        );
+    }
 
 }
